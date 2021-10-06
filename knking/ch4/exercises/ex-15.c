97bd37366a09b8e37010cbba1f4747dd039b56ce
Jubileedr <87584583+Jubileedr@users.noreply.github.com>
2021-10-06 20:04:14 +0100

    Update ex-15.c

--- knking/ch4/exercises/ex-15.c
+++ knking/ch4/exercises/ex-15.c
@@ -1,8 +1,8 @@
 /*
    a) i = 3, j = 2 ✔️
-   b) i = 0, j = 2 ✔️
-   c) i = 1, j = 2 ✔️
-   d) i = 1, j = 3 ✔️
+   b) i = 2, j = 2 ✔️
+   c) i = 2, j = 2 ✔️
+   d) i = 2, j = 3 ✔️
 */
 
 #include <stdio.h>
@@ -10,16 +10,18 @@
 int main(void)
 {
    int i, j;
+   
    i = 1, j = 2;
+   
    i += j;
    printf("a) i = %d, j = %d\n", i, j);
-   i = 1, j = 2;
+   
    i--;
    printf("b) i = %d, j = %d\n", i, j);
-   i = 1, j = 2;
+   
    i * j / i;
    printf("c) i = %d, j = %d\n", i, j);
-   i = 1, j = 2;
+   
    i % ++j;
    printf("d) i = %d, j = %d\n", i, j);
 
