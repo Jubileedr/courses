7b123d1dfec720bf994dc5b8dfd5e5f5c700ce58
Jubileedr <87584583+Jubileedr@users.noreply.github.com>
2021-11-03 19:15:43 +0000

    Update prog-5.c

--- knking/ch5/exercises/prog-5.c
+++ knking/ch5/exercises/prog-5.c
@@ -9,17 +9,17 @@ int main(void)
    if (income < 750.0f) 
       tax = 0.01f * income;
    else if (income < 2250.0f)
-      tax = 7.50f + 0.02f * income;
+      tax = 7.50f + (-(750.00f-income)*0.02f);
    else if (income < 3750.0f)
-      tax = 37.50f + 0.03f * income;
+      tax = 37.50f + (-(2250.00f-income)*0.03f) ;
    else if (income < 5250.0f)
-      tax = 82.50f + 0.04f * income;
+      tax = 82.50f + (-(3750.00f-income)*0.04f);
    else if (income < 7000.0f)
-      tax = 142.50f + 0.05f * income;
+      tax = 142.50f + (-(5250.00f-income)*0.05f);
    else 
-      tax = 230.0f + 0.06f * income;
+      tax = 230.0f +(-(7000.00f-income)*0.06f) ;
 
-   printf("You owe R%0.2f in tax\n", tax);
+   printf("You owe $%0.2f in tax\n", tax);
 
    return 0;
 }
